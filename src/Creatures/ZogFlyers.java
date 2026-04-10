b52dc3895f698dbcfbe48218f7613ded7403c92b
Christopher Jenemann <chrisjenemann@gmail.com>
2026-04-10 11:51:30 -0600

    zogFlyer now works and changes size to represent flying up and down

--- src/Creatures/ZogFlyers.java
+++ src/Creatures/ZogFlyers.java
@@ -1,6 +1,8 @@
 package Creatures;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.util.Random;
 
 import Definitions.Alien;
@@ -11,6 +13,8 @@ import Definitions.Terrain;
 public class ZogFlyers extends Alien implements CanFly
 {
     static int thesize=50;
+    String name="Flyer";
+    Color color= Color.CYAN;
 
     public ZogFlyers(int x, int y, int size){
         super("Flyer", x, y, new Random().nextInt(thesize), Color.CYAN); 
@@ -18,7 +22,28 @@ public class ZogFlyers extends Alien implements CanFly
     
     public void fly(Terrain[][] map)
     {
-        
+        Random r = new Random();
+        int nextX = gridX + (r.nextBoolean() ? 1 : -1) *r.nextInt(4);
+        int nextY = gridY + (r.nextBoolean() ? 1 : -1) *r.nextInt(4);
+
+        if(canEnter(nextX, nextY, map))
+        {
+            gridX=nextX;
+            gridY=nextY;
+            
+        }    
+    }
+    @Override
+    public void draw(Graphics g)
+    {
+        g.setColor(color);
+        int drawX = gridX * 15 + 4;
+        int drawY = gridY * 15 + 4;
+        int drawSize = new Random().nextInt(thesize) ;
+        g.fillRoundRect(drawX, drawY, drawSize, drawSize, 10, 10);
+        g.setFont(new Font("SansSerif", Font.PLAIN, 9));
+        g.setColor(Color.WHITE); 
+        g.drawString(name, drawX + 2, drawY + (drawSize / 2) + 5);
     }
 
 }

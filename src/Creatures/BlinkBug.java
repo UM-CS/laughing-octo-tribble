0c45e3a26a199b6a41c720e912e494551190f413
Aidan5685 <aidanelmasterxd@gmail.com>
2026-04-10 11:45:06 -0600

    Added walking ability to BlinkBug

--- src/Creatures/BlinkBug.java
+++ src/Creatures/BlinkBug.java
@@ -5,15 +5,17 @@ import java.util.Random;
 
 import Definitions.Alien;
 import Definitions.CanTeleport;
+import Definitions.CanWalk;
 import Definitions.Terrain;
 
-public class BlinkBug extends Alien implements CanTeleport {
+public class BlinkBug extends Alien implements CanTeleport, CanWalk {
 
     public BlinkBug(int x, int y, int size) {
         super("BlinkBug", x, y, size, Color.MAGENTA);
     }
+
     @Override
-    public void teleport(Terrain[][] map){
+    public void teleport(Terrain[][] map) {
         Random r = new Random();
 
         int newX;
@@ -22,9 +24,22 @@ public class BlinkBug extends Alien implements CanTeleport {
         do {
             newX = r.nextInt(map.length);
             newY = r.nextInt(map[0].length);
-        }  while (!canEnter(newX, newY, map));
+        } while (!canEnter(newX, newY, map));
 
         gridX = newX;
         gridY = newY;
+    }
+
+    @Override
+    public void walk(Terrain[][] map) {
+        Random r = new Random();
+
+        int nextX = gridX + (r.nextBoolean() ? 1 : -1);
+        int nextY = gridY + (r.nextBoolean() ? 1 : -1);
+
+        if (canEnter(nextX, nextY, map)) {
+            gridX = nextX;
+            gridY = nextY;
+        }
     }
 }

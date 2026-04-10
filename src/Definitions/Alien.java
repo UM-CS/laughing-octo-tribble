a7124ceda622d9d431e7fa75cfe9de717df0b29c
Sam Luther <samluther998@gmail.com>
2026-04-10 10:23:29 -0600

    added names.txt

--- src/Definitions/Alien.java
+++ src/Definitions/Alien.java
@@ -2,6 +2,7 @@ package Definitions;
 
 import java.awt.*;
 
+
 public class Alien {
     protected int gridX, gridY;
     int size;
@@ -9,7 +10,7 @@ public class Alien {
     String name;
 
     public Alien(String name, int x, int y, int size, Color color) {
-        this.name = name; 
+        this.name = name;
         this.gridX = x; 
         this.gridY = y;
         this.size = size; 
